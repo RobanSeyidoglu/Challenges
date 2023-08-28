d6b875d7bff46ba2ce14bb5b5847394556c16fa4
RobanSeyidoglu <robanseyidoglu@gmail.com>
2023-08-28 20:29:47 +0300

    Finish Challange2 With Second Method

--- JsChallange2/JsChallange2.js
+++ JsChallange2/JsChallange2.js
@@ -12,38 +12,87 @@ let TM = document.querySelectorAll(
 let TS = document.querySelectorAll(
   "table:not(.total) tr:last-child td:last-child input"
 );
+// First Method:
+
+// button.onclick = function () {
+//   let TMArray = Array.from(TM).map((input) => parseInt(input.value));
+//   let TSArray = Array.from(TS).map((input) => parseInt(input.value));
+
+//   let totalM = TMArray.reduce(function (acc, curr) {
+//     return acc + (isNaN(curr) ? 0 : curr);
+//   }, 0);
+
+//   let totalS = TSArray.reduce(function (acc, curr) {
+//     return acc + (isNaN(curr) ? 0 : curr);
+//   }, 0);
+
+//   if (totalS > totalM) {
+//     totalbig.innerHTML = totalS;
+//     totalsmall.innerHTML = totalM;
+//     namebig.innerHTML = "Sayed";
+//     namesmall.innerHTML = "Mahmoud";
+//   } else {
+//     totalbig.innerHTML = totalM;
+//     totalsmall.innerHTML = totalS;
+//     namesmall.innerHTML = "Sayed";
+//     namebig.innerHTML = "Mahmoud";
+//   }
+// };
+// reset.onclick = function () {
+//   totalbig.innerHTML = 0;
+//   totalsmall.innerHTML = 0;
+//   TM.forEach((e) => {
+//     e.value = "";
+//   });
+//   TS.forEach((e) => {
+//     e.value = "";
+//   });
+// };
+
+// Second Method:
+
+let t1 = 0;
+let t2 = 0;
 
 button.onclick = function () {
-  let TMArray = Array.from(TM).map((input) => parseInt(input.value));
-  let TSArray = Array.from(TS).map((input) => parseInt(input.value));
-
-  let totalM = TMArray.reduce(function (acc, curr) {
-    return acc + (isNaN(curr) ? 0 : curr);
-  }, 0);
-
-  let totalS = TSArray.reduce(function (acc, curr) {
-    return acc + (isNaN(curr) ? 0 : curr);
-  }, 0);
-
-  if (totalS > totalM) {
-    totalbig.innerHTML = totalS;
-    totalsmall.innerHTML = totalM;
-    namebig.innerHTML = "Sayed";
-    namesmall.innerHTML = "Mahmoud";
-  } else {
-    totalbig.innerHTML = totalM;
-    totalsmall.innerHTML = totalS;
-    namesmall.innerHTML = "Sayed";
-    namebig.innerHTML = "Mahmoud";
+  if (!button.classList.contains("alwalid")) {
+    button.classList.add("alwalid");
+    TM.forEach((e) => {
+      t1 += Number(e.value);
+    });
+    TM.forEach((e) => {
+      e.value = "";
+    });
+    TS.forEach((e) => {
+      t2 += Number(e.value);
+    });
+    TS.forEach((e) => {
+      e.value = "";
+    });
+    if (t1 > t2) {
+      totalbig.innerHTML = t1;
+      totalsmall.innerHTML = t2;
+      namesmall.innerHTML = "Mahmoud";
+      namebig.innerHTML = "Sayed";
+    } else {
+      totalbig.innerHTML = t2;
+      totalsmall.innerHTML = t1;
+      namebig.innerHTML = "Sayed";
+      namesmall.innerHTML = "Mahmoud";
+    }
   }
 };
+
 reset.onclick = function () {
+  button.classList.remove("alwalid");
   totalbig.innerHTML = 0;
   totalsmall.innerHTML = 0;
   TM.forEach((e) => {
-    e.value = "";
+    e.value = 0;
   });
   TS.forEach((e) => {
-    e.value = "";
+    e.value = 0;
   });
+  t1 = 0;
+  t2 = 0;
 };
